cd99c5cbc5a190ef6a3470595d4edaa019089e74
michail.tsougkranis@bt.com <michail,tsougkranis@gmail.com>
2020-08-11 18:09:15 +0300

    error boundary added

--- src/view/presentational/common/PrivateRoute/PrivateRoute.tsx
+++ src/view/presentational/common/PrivateRoute/PrivateRoute.tsx
@@ -3,10 +3,11 @@ import { Route, Redirect } from 'react-router-dom'
 import { PrivateRouteType } from './types'
 import PATHS from '../../../containers/Routes/PATHS'
 
+
 const  PrivateRoute  = ({ component: Component, auth: authenticated }: PrivateRouteType) =>
   <Route render={props => (
     authenticated ? (
-      <Component {...props} />
+        <Component {...props} />
       ) : (
       <Redirect to={PATHS.LOGIN}/>
       )
